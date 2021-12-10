8f5fe0eb2d32d13a9670b569823b807326a29b62
Jon1r <68221445+Jon1r@users.noreply.github.com>
2021-12-10 12:10:09 +0300

    changes fo Heroku

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,12 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+
+import config
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=types.ParseMode.HTML)
+#bot = Bot(token=os.getenv('TOKEN'), parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
